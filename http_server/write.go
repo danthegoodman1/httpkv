3d8d763d941c1c3c46ca86834437af3adb91b06b
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2024-06-22 11:34:29 -0400

    param wrangling

--- http_server/write.go
+++ http_server/write.go
@@ -2,10 +2,25 @@ package http_server
 
 import (
 	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog"
 	"net/http"
 )
 
+type WriteKeyParams struct {
+	Key string `param:"key" validate:"lte=1024"`
+
+	NotExists *string `query:"nx"`
+	IfExists  *string `query:"ix"`
+}
+
 func (s *HTTPServer) WriteKey(c *CustomContext) error {
-	key := c.Param("key")
-	return c.String(http.StatusAccepted, fmt.Sprintf("Writing key: %s", key))
+	var params WriteKeyParams
+	if err := ValidateRequest(c, &params); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	logger := zerolog.Ctx(c.Request().Context())
+	logger.Debug().Interface("params", params).Msg("Params")
+
+	return c.String(http.StatusAccepted, fmt.Sprintf("Writing key: %s", params.Key))
 }
